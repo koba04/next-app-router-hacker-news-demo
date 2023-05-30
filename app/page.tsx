6f2e197ce69ad5908b497158b21d5ecf1e347c66
Toru Kobayashi <koba0004@gmail.com>
2023-05-30 19:45:06 +0900

    feat: get all data from the database

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image'
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
-export default function Home() {
+export default async function Home() {
+  const db = await open({
+    filename: './db/database.db',
+    driver: sqlite3.Database
+  });
+  const result = await db.all('SELECT title, url from bookmark');
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -27,7 +36,11 @@ export default function Home() {
           </a>
         </div>
       </div>
-
+      <ul>
+        {result.map(({ title, url }) => (
+          <li key={url}><a href={url}>{title}</a></li>
+        ))}
+      </ul>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
