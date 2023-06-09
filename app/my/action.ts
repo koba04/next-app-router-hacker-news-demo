9c79fbe8b8ba260bbb159a2b609bdf46c5d239bf
Toru Kobayashi <koba0004@gmail.com>
2023-06-10 00:48:08 +0900

    refactor: move "use server" into the top level

--- app/my/action.ts
+++ app/my/action.ts
@@ -1,8 +1,9 @@
+'use server';
+
 import { revalidatePath } from 'next/cache';
 import { removeBookmark } from '../repository';
 
 export async function remove(formData: FormData) {
-  'use server';
   const id = Number(formData.get("id"));
   await removeBookmark(id);
   revalidatePath("/my");
