1e341adaefa0e5646e91adcc2a9e52a344389024
Toru Kobayashi <koba0004@gmail.com>
2023-06-27 13:48:15 +0900

    refactor: use shadow-md instead of box-shadow-md

--- app/_components/BookmarkRow.tsx
+++ app/_components/BookmarkRow.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export const BookmarkRow = ({ link, action, comment } : { link: ReactNode, action: ReactNode, comment: string }) => (
-  <div className="p-2 text-xl border-violet-200 border-2 w-4/5 hover:drop-shadow-md">
+  <div className="p-2 text-xl border-violet-200 border-2 w-4/5 hover:shadow-md">
     <div className="flex">
       <div className="flex-grow">{link}</div>
       <div className="inline-block text-sm px-2">{action}</div>
