07a0751bbac258b6687d320d3c3381669074d20f
Toru Kobayashi <koba0004@gmail.com>
2024-02-23 14:05:52 +0900

    chore: update deps and use corepack

--- app/_components/SubmitButton.tsx
+++ app/_components/SubmitButton.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 import { Button } from "./Button";
 
 export const SubmitButton = ({ label, pendingLabel }: { label: string, pendingLabel: string }) => {
